a26cfaa5936cc09117a1ec21e3a17a40d6961deb
liyignhao <zkht_liyinghao@gomeplus.com>
2018-01-20 18:28:59 +0800

    初识router

--- app/src/index.js
+++ app/src/index.js
@@ -4,8 +4,10 @@ import App from './App';
 import {Provider} from 'react-redux';
 import configureStore from './redux/store'
 import registerServiceWorker from './registerServiceWorker';
+import * as router from './router';
+console.log(router)
 
 const store = configureStore();
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>{ router.route }</Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ app/src/router/index.js
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Route, Link, BrowserRouter } from 'react-router-dom';//react-router 4.0
+import React, { Component } from 'react';
+import App from '../App';
+
+export const route = (
+    <Router>
+        <Route path="/" component={ App }></Route>
+    </Router>
+)
